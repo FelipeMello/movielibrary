a7000540b0708a797d09543c6dd478ea40614805
felipeMello <19962152+FelipeMello@users.noreply.github.com>
2022-08-28 12:28:08 +0100

    movie library first commit

---
+++ src/test/java/fsdm/movie/library/MovieLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package fsdm.movie.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
